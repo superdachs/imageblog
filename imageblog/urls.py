147f36f44d710cce562b21a2c947387aeeb2d43a
Stefan Kauerauf <mail@stefankauerauf.de>
2014-04-14 22:13:50 +0200

    csnjk

--- imageblog/urls.py
+++ imageblog/urls.py
@@ -10,7 +10,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'imageblog.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^/', include('core.urls')),
-#    url(r'^sites/', include('core.urls')),
+    url(r'^sites/', include('core.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
