fecbfe86fc28770dcce552d7c71f017d4edeb5e4
Stefan Kauerauf <mail@stefankauerauf.de>
2014-04-14 23:31:01 +0200

    cnskl

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,6 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^(?P<site_id>\d+)/$', views.site, name='site'),
     url(r'^(?P<site_id>\d+)/gallery/(?P<gallery_id>\d+)/detail/(?P<image_id>\d+)/$', views.gallery, name='gallery'),
-     url(r'^gallery/(?P<gallery_id>\d+)/detail/(?P<image_id>\d+)/$', views.gallery, name='gallery'),
    url(r'^(?P<site_id>\d+)/gallery/(?P<gallery_id>\d+)/overview/$', views.gallery_overview, name='gallery_overview'),   
     url(r'^(?P<site_id>\d+)/article/(?P<article_id>\d+)/$', views.article, name='article'),
 )
