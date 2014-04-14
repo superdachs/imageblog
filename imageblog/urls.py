bcf1ec1cc240563c6df43c21eadc0e132bb8309f
Stefan Kauerauf <mail@stefankauerauf.de>
2014-04-14 22:20:02 +0200

    A

--- imageblog/urls.py
+++ imageblog/urls.py
@@ -11,5 +11,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'imageblog.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^sites/', include('core.urls')),
+    url(r'^$', core.views.index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
