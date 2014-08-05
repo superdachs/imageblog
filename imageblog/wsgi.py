6faafbf0cc70379f1c6a44d6a44e72514264ac8d
Stefan Kauerauf <mail@stefankauerauf.de>
2014-08-05 19:50:39 +0200

    gi Bitte geben Sie eine Commit-Beschreibung für Ihre Änderungen ein. Zeilen,

--- imageblog/wsgi.py
+++ imageblog/wsgi.py
@@ -8,7 +8,10 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import sys
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "imageblog.settings")
+
+sys.path.append('/srv/http/dachsbau-alpha.de/imageblog')
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
