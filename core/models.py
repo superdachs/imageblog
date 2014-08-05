0b96597b7981470179bcfe040c4e74a86faf0e56
Stefan Kauerauf <mail@stefankauerauf.de>
2014-08-05 23:50:18 +0200

    test

--- core/models.py
+++ core/models.py
@@ -19,6 +19,7 @@ class Article(models.Model):
     description = models.TextField()
     pub_date = models.DateTimeField('date published')
     content = models.TextField()
+    author = models.CharField(max_length=3, default='nbd')
 
     def __unicode__(self):
         return self.title
